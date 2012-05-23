5b0e3e886be972a3f4b7d1a0199c5943cd11b85b
Mike Kruzil <mkruzil@gmail.com>
2012-05-23 12:16:39 -0400

    changed application object to closure

--- README.md
+++ README.md
@@ -2,8 +2,3 @@ Character Counter
 =================
 
 A Twitter-style character counter
-
-To Fix
-======
-1. 'this' refers to DOM object (not JS object) during event execution
-2. Existing text box characters are not 'seen' after page refreshes in IE
--- counter.js
+++ counter.js
@@ -26,56 +26,61 @@ SOFTWARE.
 
 */
 
-window.counter = {
-    textBox : window.document.getElementById("textBox"),
-    display : window.document.getElementById("display"),
+(function () {
+
+    "use strict";
+
+    //DOM Elements
+    var TEXTBOX = window.document.getElementById("textbox");
+    var DISPLAY = window.document.getElementById("display");
 
     //Stores the maximum number of characters allowed in the text box
-    charLimit : 140,
+    var LIMIT = 140;
 
     //Gets the number of characters the user has remaining in the text box before the character limit is reached.
-    getRemainingChars : function () {
-        var charLength = window.counter.textBox.value.length;
-        var charLimit = window.counter.charLimit;
+    function getRemainingChars() {
+        var length = TEXTBOX.value.length;
         /*
             If the text box is empty, then there are no characters in the text box, which means the character limit should be displayed to the user.
             Otherwise, subtract the current length from the character limit and display that number to the user.
         */
-        return !charLength ? charLimit : charLimit - charLength;
-    },
-
-    //Appends the current number of characters in the text box
-    setDisplay : function () {
-        var text = String(window.counter.getRemainingChars());
-        var textNode = {};
-        var container = window.counter.display;
-        var i = 0;
-        textNode = window.document.createTextNode(text);
-        /* 
-           The textNode should be appended to an empty container.
-           So, we remove each node currently inside the container, starting with the last node in the NodeList and ending when 0 is reached.
-           Note: Since childNodes is a live list(i.e, removal of items is instant), the length must be called repeatedly during the loop in order to get the current value.
-        */
-        i = container.childNodes.length;
-        while (container.childNodes.length > 0) {
-            container.removeChild(container.childNodes.item(--i));
-        }
-        container.appendChild(textNode);
-    },
-
-    setKeyEvents : function () {
-        var textBox = this.textBox;
-        var setDisplay = this.setDisplay;
-        textBox.onkeydown = setDisplay;
-        textBox.onkeypress = setDisplay;
-        textBox.onkeyup = setDisplay;
-    },
-
-    init : function () {
-        this.setKeyEvents();
-        this.setDisplay();
+        return !length ? LIMIT : LIMIT - length;
     }
 
-};
+    //Appends the current number of characters in the text box
+    function setDisplay() {
+        var chars = getRemainingChars();
+        var textNode = window.document.createTextNode(String(chars));
+        var i = 0;
+        /* 
+           The text node should be appended to an empty container.
+           So, we remove each node currently in the container node, starting with the last node in the NodeList and ending when 0 is reached.
+           Note: Since childNodes is a live list(i.e, removal of items is instant), the length must be called repeatedly during the loop in order to get an up-to-date value.
+        */
+        i = DISPLAY.childNodes.length;
+        while (DISPLAY.childNodes.length > 0) {
+            DISPLAY.removeChild(DISPLAY.childNodes.item(--i));
+        }
+        DISPLAY.appendChild(textNode);
+        //If the character limit has been exceeded, color the display red.
+        DISPLAY.style.color = chars < 0 ? "red" : "";
+    }
 
-window.counter.init();
+    function init() {
+        //If there isn't already an onload function set, set one that clears any value currently in the text box (mainly for IE)
+        if (window.onload === null) {
+            window.onload = function () {
+                TEXTBOX.value = "";
+            };
+        }
+        //Set key events for the text box
+        TEXTBOX.onkeydown = setDisplay;
+        TEXTBOX.onkeypress = setDisplay;
+        TEXTBOX.onkeyup = setDisplay;
+        //Set the display to its initial value
+        setDisplay();
+    }
+
+    init();
+
+}());
